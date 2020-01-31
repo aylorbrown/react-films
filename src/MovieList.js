854aadda0211f72461778953daf171ad56e752ba
Aylor Brown <aylorbrown@gmail.com>
2020-01-31 14:04:25 -0500

    finishes lesson in react classes

---
+++ src/MovieList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MovieList() {
+    return (
+        <ul>
+            <li>The Conjuring</li>
+            <li>The Shining</li>
+            <li>Hereditary</li>
+        </ul>
+    )
+}
+
+export default MovieList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Hero {
+ //properties or attributes 
+ // "variables that belong to each object"
+    constructor(health=18, power=5) {
+        this.health = health; 
+        this.power = power; 
+    }
+ // behaviors or methods 
+ // "functions that belong to each instance"
+ hug(otherHero) {
+    //  otherHero.health += this.power; 
+     otherHero.receiveHealth(this.power);
+    //  console.log(`otherHero has ${otherHero.health}`);
+ }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ receiveHealth(healthPoints) {
+     this.health += healthPoints; 
+     console.log(`Yay I love hugs. I have ${this.health}`);
+ }
+}
+
+const aHero = new Hero(); 
+// console.log(aHero);
+const anotherHero = new Hero();
+aHero.hug(anotherHero);
+aHero.hug(anotherHero);
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
